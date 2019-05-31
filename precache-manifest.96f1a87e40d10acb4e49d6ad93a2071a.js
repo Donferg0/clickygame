15581b6efb7a683267248a1402a9f0a92995518e
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-05-31 16:42:33 -0400

    Updates

---
+++ precache-manifest.96f1a87e40d10acb4e49d6ad93a2071a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b79d920983d51c6be74fbbaec8ca785",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "d50380f2003cc2a464ca",
+    "url": "/clickygame/static/css/main.0ac7d0f8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickygame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d50380f2003cc2a464ca",
+    "url": "/clickygame/static/js/main.d1661163.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "7a91e4e1df461a2fcc8b229a354adeb2",
+    "url": "/clickygame/static/media/1.7a91e4e1.jpg"
+  },
+  {
+    "revision": "c007a8ab63d8a1a666d12e22112e250a",
+    "url": "/clickygame/static/media/2.c007a8ab.jpg"
+  }
+]);
